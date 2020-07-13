84ae060ba0efe1d1086ca6c4a08f58687407a274
AnnaSemovskaya <68237642+AnnaSemovskaya@users.noreply.github.com>
2020-07-13 18:05:51 +0300

    Add files via upload

---
+++ testJS.js
@@ -0,0 +1,38 @@
+function redButtonClick() {
+
+    var canvas = document.getElementById("myCanvas");
+
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillRect(25,25,100,100);
+        //ctx.clearRect(45,45,60,60);
+        //ctx.strokeRect(50,50,50,50);
+    }
+}
+
+function blueButtonClick() {
+
+    var canvas = document.getElementById("myCanvas");
+
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        //ctx.fillRect(25,25,100,100);
+        ctx.clearRect(45,45,60,60);
+        //ctx.strokeRect(50,50,50,50);
+    }
+}
+
+function greenButtonClick() {
+
+    var canvas = document.getElementById("myCanvas");
+
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        //ctx.fillRect(25,25,100,100);
+        //ctx.clearRect(45,45,60,60);
+        ctx.strokeRect(50,50,50,50);
+    }
+}
